b03ce00ad0b3ef7d2e77d0594f869f3d1449f4c5
Kobi Medrish <kobi.medrish@gmail.com>
2022-12-08 18:28:15 +0200

    Added home-manager as a module inside of system configuration.

--- flake.nix
+++ flake.nix
@@ -3,8 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-22.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs }: 
+  outputs = { self, nixpkgs, home-manager }: 
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -17,7 +21,15 @@
       nixosConfigurations = {
         kvm-nixos = lib.nixosSystem {
 	  inherit system;
-	  modules = [ ./configuration.nix ];
+	  modules = [ 
+	  ./configuration.nix 
+	  home-manager.nixosModules.home-manager
+	  {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.users.drone = import ./home.nix;
+	  }
+	  ];
 	};
       };
     };
