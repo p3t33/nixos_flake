d5c007fbed24b22cb571c0d503c72340ac3ac3e0
kobi <kobi.medrish@gmail.com>
2022-12-11 16:43:19 +0200

    Changes to login - By default user user will be loged with no need for   password and into i3.

--- os/services/xserver.nix
+++ os/services/xserver.nix
@@ -11,6 +11,10 @@
     displayManager = {
       lightdm.enable = true;
       defaultSession = "none+i3";
+      autoLogin = {
+        enable = true;
+        user = "kmedrish";
+      };
     };
 
     desktopManager = {
