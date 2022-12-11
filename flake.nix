326c59cbe4af9d40fe7dfd0da1262c6b7a2c3dc2
kobi <kobi.medrish@gmail.com>
2022-12-11 12:26:24 +0200

    refactoring

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
 	  {
 	    home-manager.useGlobalPkgs = true;
 	    home-manager.useUserPackages = true;
-	    home-manager.users.drone = import ./vm_home.nix;
+	    home-manager.users.drone = import ./home/vm_home;
 	  }
 	  ];
 	};
