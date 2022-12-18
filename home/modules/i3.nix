d28fc50ea7a97fb1e71ad08062e54bcb6f105765
Kobi Medrish <kobi.medrish@gmail.com>
2022-12-18 18:14:53 +0200

    Added volume keybindings to i3

--- home/modules/i3.nix
+++ home/modules/i3.nix
@@ -148,7 +148,10 @@ in {
         # exit i3 (logs you out of your X session)
         "${mod}+Shift+q" = "exec \"i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -B 'Yes, exit i3' 'i3-msg exit'\"";
 
-
+        "XF86AudioRaiseVolume" =  "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +10%";
+        "XF86AudioLowerVolume" = "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -10%";
+        "XF86AudioMute" = "exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle";
+        "XF86AudioMicMute" = "exec --no-startup-id pactl set-source-mute @DEFAULT_SOURCE@ toggle";
       };
 
       # i3wm like other tiling window managers defines where and how bars are
