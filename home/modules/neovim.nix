447ac9aa0864ec577a4055a953b4dd40cdb56cec
Kobi Medrish <kobi.medrish@gmail.com>
2022-12-26 12:16:12 +0200

    neovim navigation update
    - easier navigation between windows.

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -47,6 +47,11 @@
 
       nnoremap <C-u> <C-u>zz
       nnoremap <C-d> <C-d>zz
+
+      nmap <silent> <c-k> :wincmd k<CR>
+      nmap <silent> <c-j> :wincmd j<CR>
+      nmap <silent> <c-h> :wincmd h<CR>
+      nmap <silent> <c-l> :wincmd l<CR>
       ";
     };
 
