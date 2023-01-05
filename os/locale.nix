8f98c3b53c9127ce5f25f27197c7526595ebdaec
Kobi Medrish <kobi.medrish@gmail.com>
2023-01-05 15:50:51 +0200

    Refactored locale settings

---
+++ os/locale.nix
@@ -0,0 +1,20 @@
+{ pkgs,  ... }:
+{
+#   Set your time zone.
+    time.timeZone = "Asia/Jerusalem";
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_IL";
+
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "he_IL.UTF-8";
+        LC_IDENTIFICATION = "he_IL.UTF-8";
+        LC_MEASUREMENT = "he_IL.UTF-8";
+        LC_MONETARY = "he_IL.UTF-8";
+        LC_NAME = "he_IL.UTF-8";
+        LC_NUMERIC = "he_IL.UTF-8";
+        LC_PAPER = "he_IL.UTF-8";
+        LC_TELEPHONE = "he_IL.UTF-8";
+        LC_TIME = "he_IL.UTF-8";
+    };
+}
