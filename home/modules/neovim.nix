de3ce6bc38456acc377e35a83e17f76db3738086
Kobi Medrish <kobi.medrish@gmail.com>
2023-02-12 19:40:22 +0200

    Updated nvim theme

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -313,26 +313,7 @@
 
       # Themes
       # ------
-      {
-          plugin = rose-pine;
-          type = "lua";
-          config = ''
-              require('rose-pine').setup({
-                      disable_background = true
-                      })
-
-          function ColorMyPencils(color) 
-              color = color or "rose-pine"
-              vim.cmd.colorscheme(color)
-
-              vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-              vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
-              end
-
-              ColorMyPencils()
-          '';
-      }
+      vim-code-dark
       dracula-vim
       nord-vim
       gruvbox
@@ -464,7 +445,7 @@
       set notimeout nottimeout
 
 
-      colorscheme rose-pine
+      colorscheme codedark
       
       "line numbers and relative lines"
       set number
