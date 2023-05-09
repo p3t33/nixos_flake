059306d1186f1de46db582fe61a44981666034d4
Kobi Medrish <kobi.medrish@gmail.com>
2023-05-09 21:44:50 +0300

    Added home-desktop host
    - Updated global variables with the new host.
    - Updated flake with the new host.
    - Added host specific software such as signal-desktop.
    - Added support for ntfs(for home-desktop host only).

---
+++ hosts/home_desktop/configuration-services.nix
@@ -0,0 +1,11 @@
+{ ... }:
+{
+
+  imports = [
+    ../../os/services/xserver.nix
+    ../../os/services/udev.nix
+    ../../os/services/user/tmux.nix
+    ../../os/services/system/moolticuted.nix
+    ../../os/services/cups.nix
+  ];
+}
