ad3a54bf39ef7e0d52a9c060f8a2adb350eed2d6
Kobi Medrish <kobi.medrish@gmail.com>
2023-05-10 11:24:56 +0300

    Removed not used audio package

--- hosts/work_pc/configuration.nix
+++ hosts/work_pc/configuration.nix
@@ -82,7 +82,6 @@
     moolticute
     polybar
 
-    pulseaudio
     syncthing
     nitrogen # A wallpaper browser and setter for X11
     picom # A fork of XCompMgr, a sample compositing manager for X servers
