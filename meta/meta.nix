a307e9bc7d3f1d4960e9e97bf7b3841558d0c995
Kobi Medrish <kobi.medrish@gmail.com>
2023-05-15 10:42:43 +0300

    Refactored colors into a global variable
    Color are now defined as a global variable and can
    now be used across software that until defined its own.
    This change removes one more piece of code reuse.

--- meta/meta.nix
+++ meta/meta.nix
@@ -52,6 +52,39 @@
         default = "changeme";
     };
 
+    colors = mkOption {
+        type = attrsOf str;
+        default = {
+            background = "#312f2f";
+            background-alt = "#3b4354";
+            foreground = "#F1FAEE";
+            primary = "#08D9D6";
+            secondary = "#047672";
+            alert = "#ff2e63";
+            disabled = "#707880";
+            bg = "#2f343f";
+            inactive-bg = "#2f343f";
+            text = "#f3f4f5";
+            inactive-text = "#676E70";
+            urgent-bg = "#E53935";
+      };
+        example = {
+            background = "#312f2f";
+            background-alt = "#3b4354";
+            foreground = "#F1FAEE";
+            primary = "#08D9D6";
+            secondary = "#047672";
+            alert = "#ff2e63";
+            disabled = "#707880";
+            bg = "#2f343f";
+            inactive-bg = "#2f343f";
+            text = "#f3f4f5";
+            inactive-text = "#676E70";
+            urgent-bg = "#E53935";
+        };
+      description = "The color palette for the user interface.";
+    };
+
     };
   };
 
