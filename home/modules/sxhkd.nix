2b304ae59b3cc9bd1266708a4b91084564e410b1
Kobi Medrish <kobi.medrish@gmail.com>
2023-06-27 21:34:48 +0300

    Fixed volume control key bindings
    Bindings were broken because I moved to alsa so I
    needed to switch from pactl to amixer.
    
    I also moved from using i3wm hotkeys daemon for volume
    control to sxhkd daemon for a better "single responsibility".
    This way I decoupled a generic use case from my window
    manager that might change in the future.

--- home/modules/sxhkd.nix
+++ home/modules/sxhkd.nix
@@ -6,32 +6,32 @@
    #
    # There a few ways to autostart sxhkd as
    # a daemon.
-   # 
+   #
    # 1. Using systemd. This can be done by writing a user service
    #    that will go into configuration.nix(systemd.user.services.sxhkd).
    #    It can also be done by using the home-manger(services.sxhkd.enable = true;)
    #    For some reason using home-manger doesn't work for me. unit file isn't
-   #    being created. But the problem with systemd services is that it 
-   #    by design intended to work in isolation, it doesn't 
+   #    being created. But the problem with systemd services is that it
+   #    by design intended to work in isolation, it doesn't
    #    inherit the user environment which means that it isn't very good
    #    choise for deamons that are used to execute other executables(E.g tmux/sxhkd).
    #    this is why I only use to home-manger to create settings file for sxhkd and
    #    nothing else.
    # 2. Execute the binary from the i3wm, which is what I will be doing.
-   # 
+   #
    #
    # Notes for rofi key bindings
    # ---------------------------
-   # Once available modes are defined in the rofi config.rasi executing a 
+   # Once available modes are defined in the rofi config.rasi executing a
    # model can be written in a short manner(E.g "rofi -show ssh" instead of
    # "rofi -modi ssh -show ssh") The reason I decided to use the longer version
-   # is in order to decapule config.rasi from the rofi command. Because if a 
+   # is in order to decapule config.rasi from the rofi command. Because if a
    # model is removed from the config.rasi and a shorter version is used it will
    # brake.
    #
    # Also non builtin modes(just executables) need a bit of a different syntax
    # ssh, emoji, and calc are builtin modes while power-menu and firefox-bookmarks
-   # are not. 
+   # are not.
 
    home.file = {
      ".config/sxhkd/sxhkdrc".text = ''
@@ -44,7 +44,7 @@
 
        super + c
          rofi -modi calc -show calc
-       
+
        super + p
          rofi -modi power-menu:rofi-power-menu -show power-menu
 
@@ -53,6 +53,18 @@
 
        Print
          flameshot gui
+
+       XF86AudioRaiseVolume
+         amixer sset Master 5%+
+
+       XF86AudioLowerVolume
+         amixer sset Master 5%-
+
+       XF86AudioMute
+         amixer sset Master toggle
+
+       XF86AudioMicMute
+         amixer set Capture toggle
        '';
    };
 
