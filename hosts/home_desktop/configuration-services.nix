1e0896aef03dfcd61b0ba27a82631ef37d9b73c7
Kobi Medrish <kobi.medrish@gmail.com>
2023-08-13 13:17:47 +0300

    Refactored moolticuted
    Logically udev rules shouldn't be grouped together, as each one is specific
    to some device and not all rules are required by hosts that may include
    the udev.nix file. And so udev.nix was removed.
    
    Some devices might have a companion app that needs more then a daemon
    running but also some udev rule. A good example is the moolticuted.
    So in such case it makes sense to couple the udev rule and the systemd
    service(daemon) into a single .nix file as the two essentially a single
    unit.

--- hosts/home_desktop/configuration-services.nix
+++ hosts/home_desktop/configuration-services.nix
@@ -3,7 +3,6 @@
 
   imports = [
     ../../os/services/xserver.nix
-    ../../os/services/udev.nix
     ../../os/services/user/tmux.nix
     ../../os/services/system/moolticuted.nix
     ../../os/services/cups.nix
