331ece2a7df281e42755ec13c478c7ce04657612
Kobi Medrish <kobi.medrish@gmail.com>
2023-08-15 14:10:05 +0300

    Tmux will now automatically restore saved session on boot
    - Added directives to continuum and restore tmux plugins so
    when tmux server starts it will automatically restore(using continuum
    plugin) the sessions that were saved(using restore plugin).
    - Fixed systemd tmux unit to start tmux with "start-server" switch so
    when started no new sessions will be created(as it was when using
    "new-session -d" switches) This way tmux will be start on boot
    it will restore saved sessions any nothing else.
    
    Note: the systemd aspect of tmux is defined in a seperate .nix file
    and not generated by the continuum plugin settings, as this is NixOS
    and systemd units hare handled as nix setting files.

--- os/services/user/tmux.nix
+++ os/services/user/tmux.nix
@@ -11,7 +11,7 @@
       Type = "forking";
       #Environment = "PATH=/run/wrappers/bin:/home/${user}/.nix-profile/bin:/etc/profiles/per-user/${user}/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin";
       #ExecStart="${pkgs.tmux}/bin/tmux new-session -d";
-      ExecStart="${pkgs.bash}/bin/bash -c 'source ${config.system.build.setEnvironment} ; exec ${pkgs.tmux}/bin/tmux new-session -d'";
+      ExecStart="${pkgs.bash}/bin/bash -c 'source ${config.system.build.setEnvironment} ; exec ${pkgs.tmux}/bin/tmux start-server'";
       ExecStop="${pkgs.tmux}/bin/tmux kill-server";
     };
 
