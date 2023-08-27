9547b56b99540481d04b9f1407a08361fd28a437
Kobi Medrish <kobi.medrish@gmail.com>
2023-08-27 18:14:21 +0300

    Added clipmenu clip board history daemon
    This daemon boots with the OS and uses xclip to get its
    clip board history.
    
    I also updated sxhkd hot key daemon to include clipmenu
    so I can access it via a key binding.

--- hosts/work_pc/home.nix
+++ hosts/work_pc/home.nix
@@ -56,6 +56,7 @@
     ../../home/modules/nix-index.nix
     ../../home/modules/atuin.nix
     ../../home/modules/lazygit.nix
+    ../../home/modules/clipmenu.nix
   ];
 
   home.sessionVariables = {
