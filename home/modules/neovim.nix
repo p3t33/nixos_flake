e1b1afafeb6b24b046b0d8df8d8d67e421a1d50e
Kobi Medrish <kobi.medrish@gmail.com>
2023-08-27 17:06:34 +0300

    Added keybinding to neovim harpoon plugin
    Now I am able to go between marked files using
    leader+[1..4]

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -451,8 +451,10 @@
           vim.keymap.set("n", "<leader>a", mark.add_file)
           vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-          vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-          vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
+          vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
+          vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
+          vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
+          vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
         '';
       }
     ];
