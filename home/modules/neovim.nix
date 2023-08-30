ee88e23f0e5eccaf79082de5b29654f4aca267ad
Kobi Medrish <kobi.medrish@gmail.com>
2023-08-30 19:04:49 +0300

    Added gitsign plugin to neovim
    I am mostly using the option to toggle git blame,
    and the ability to move from one changed hunk to the
    next.

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -13,7 +13,22 @@
         plugin = gitsigns-nvim;
         type = "lua";
         config = ''
-            require('gitsigns').setup()
+            require('gitsigns').setup {
+                on_attach = function(bufnr)
+                local gs = package.loaded.gitsigns
+                    vim.keymap.set('n', "<leader>gt", gs.toggle_current_line_blame)
+                    vim.keymap.set('n', "]g", function()
+                            if vim.wo.diff then return ']c' end
+                            vim.schedule(function() gs.next_hunk() end)
+                            return '<Ignore>'
+                            end, {expr=true})
+                    vim.keymap.set('n', "[g", function()
+                            if vim.wo.diff then return '[c' end
+                            vim.schedule(function() gs.prev_hunk() end)
+                            return '<Ignore>'
+                            end, {expr=true})
+            end
+            }
         '';
       }
       {
@@ -645,5 +660,4 @@
       noremap <Right> <Nop>
       '';
     };
-
 }
