49dbf40ab9eed0fdf9ff93aae878120ff2e52fa9
Kobi Medrish <kobi.medrish@gmail.com>
2023-09-07 18:18:56 +0300

    Disabled the use of the built in plugin manager in emacs
    This should prevent conflicts with the nix based plugin manager.

--- home/modules/emacs.nix
+++ home/modules/emacs.nix
@@ -9,9 +9,24 @@
 
         ];
         extraConfig = ''
+            ;; disabling the default built int plugin manager
+            ;; in context of nix these settings are essentially ensuring a
+            ;; clean slate" in terms of package management behavior in Emacs
+            ;; They ensure that certain built-in package manager features that
+            ;; might conflict with an external management tool are turned
+            ;; off or are in their most permissive settings.
+            ;;
+            ;; This should be always at the top of the file before any
+            ;; other settings have the chance to run.
+            (setq package-quickstart nil)
+            (setq package-menu-async nil)
+            (setq package-load-list '(all))
+
             (setq standard-indent 2)
             (load-theme 'nord t)
             (message "Loading init.el...")
+
+
         '';
     };
 
