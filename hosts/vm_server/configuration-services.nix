7096bdb10663bd1aba3608709a123dac592d5346
Kobi Medrish <kobi.medrish@gmail.com>
2023-09-21 15:55:06 +0300

    Added watchman as user systemd service

--- hosts/vm_server/configuration-services.nix
+++ hosts/vm_server/configuration-services.nix
@@ -3,6 +3,9 @@
 
   imports = [
     ../../os/services/sshd.nix
+    ../../os/services/user/tmux.nix
+    ../../os/services/system/moolticuted.nix
+    ../../os/services/user/watchman.nix
   ];
 
 }
