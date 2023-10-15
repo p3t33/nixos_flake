b838ac8236c38ec16993d0cceeeb26a855eadd09
Kobi Medrish <kobi.medrish@gmail.com>
2023-10-15 16:19:00 +0300

    Refactoring starship prompt
    In order to create a more informative command line prompt
    I refactored the settings for starship prompt and provided it
    a look that reminds the "lean style" used by Powerlevel10k.
    
    I decided to stick with starship as it is not limited to zsh
    and can be reused with other shells such as bash which is the
    "default" when it comes to remote servers.

--- home/modules/starship.nix
+++ home/modules/starship.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 
 {
 
@@ -9,74 +9,181 @@ programs.starship = {
       settings = {
 
         command_timeout = 2000; # in milliseconds
+        # new line above the prompt.
         add_newline = false;
-        username = {
-          style_user = "green bold";
-          style_root = "red bold";
-          format = "[$user]($style)";
-          disabled = false;
-          show_always = true;
+        scan_timeout = 10;
+
+        # Order of right side modules of the prompt
+        #
+        # Creating elements that blend
+        # ----------------------------
+        # The idia is to set a backgroung for an element
+        # then use the triangle which will have the foreground of the
+        # previus elemnt and the background of the following elment.
+        # and put the triangle as an elemnt in the order, for example
+        # "directory"
+        # "[](fg:#3B76F0 bg:#FCF392)"
+        # "git_branch"
+        # with directory background being #3B76F0 and the git_branch background
+        # being #FCF392
+        #
+        # Another cool trick is to creat rounded corners using the same principle
+        # for example: "[](bg:#030B16 fg:#7DF9AA)" that you put in the begining of the
+        # format order here is a commentedf out more detalied example:
+        # format = lib.concatStrings [
+        #    "[](bg:#030B16 fg:#7DF9AA)"
+        #    "$directory"
+        #    "$c"
+        #    "[](fg:#3B76F0 bg:#FCF392)"
+        #    "$git_branch"
+        #    "$git_status"
+        #    "[](fg:#FCF392 bg:#030B16)"
+        #    "$fill" # This will push the right prompt to the right edge
+        #    "$cmd_duration"
+        #    "$time"
+        #    "$status"
+        #    "$line_break"
+        #    "$character"
+        #];
+        format = lib.concatStrings [
+            "$os"
+            "$hostname"
+            "$directory"
+            "$python"
+            "$c"
+            "$git_branch"
+            "$git_status"
+            "$fill" # This will push the right prompt to the right edge
+            "$cmd_duration"
+            "$time"
+            "$status"
+            "$line_break"
+            "$character"
+        ];
+
+        # Only used when setting a single left side prompt, otherwise fill moudle
+        # should be used.
+        #
+        # Order of right side modules of the prompt
+        right_format = lib.concatStrings [
+        ];
+
+        # Used to align/create "left and right prompt on the same line when two
+        # line prompt is is used.
+        fill = {
+            symbol = " ";
+            style = "bold green";
         };
 
-        hostname = {
-          ssh_only = false;
-          format = "@[$hostname](bold yellow):";
-          trim_at = ".";
-          disabled = false;
+
+        time = {
+            disabled = false;
+            time_format = "%R";
+            style = "bg:#1d2230";
+            format = "[ 󱑍 $time]($style)";
         };
-        character = {
-          success_symbol = "[❯](bold green)";
-          error_symbol = "[✗](bold red)";
+
+        # creates the two line prompt.
+        line_break = {
+            disabled = false;
         };
 
         directory = {
-          read_only = " ";
+          read_only = "🔒"; # icon with some blank space
           truncation_length = 10;
+          truncation_symbol = "…/";
           truncate_to_repo = true;
-          style = "bold italic blue";
+
+          format = "[ [$read_only]($read_only_style)  $path]($style)";
+          read_only_style = "red";
+          style = "fg:#00B0FF";
         };
 
-        cmd_duration = {
-          min_time = 2;
-          show_milliseconds = false;
-          disabled = false;
-          style = "bold italic red";
-        };
-
-        aws = {
-          symbol = "  ";
-        };
 
         git_branch = {
-          symbol = " ";
+            symbol = "   ";
+            style = "fg:#5FD700";
+            format = "[$symbol$branch(:$remote_branch)]($style)";
+        };
+
+        # Might be slow on windows.
+        git_status = {
+            format = "[ $all_status$ahead_behind]($style)";
+            style = "fg:#5FD700";
+        };
+
+
+
+        cmd_duration = {
+            format = "[ took 󱦟 $duration at]($style)";
+            style = "bg:#1d2230";
+            min_time = 500;
+        };
+
+        character = {
+            success_symbol = "[ ➜](#5FD700)";
+            error_symbol = "[ ✗](#E84D44)";
+            vimcmd_symbol = "[ ❮](bold green)";
+            vimcmd_replace_one_symbol = "[ ❮](bold purple)";
+            vimcmd_replace_symbol =  "[ ❮](bold purple)";
+            vimcmd_visual_symbol =  "[ ❮](bold yellow)";
+            disabled = false;
+        };
+
+        hostname = {
+            ssh_only = true;
+            format = "[$hostname $ssh_symbol]($style)";
+            ssh_symbol = "";
+            style = "bold dimmed green";
+            disabled = false;
         };
 
         golang = {
-          symbol = " ";
+            symbol = " ";
         };
 
         rust = {
-          symbol = " ";
+            symbol = " ";
         };
 
         c = {
-          symbol = " ";
+            symbol = " ";
+            format = "[ $symbol($version(-$name) )]($style)";
         };
 
         nix_shell = {
-          symbol = " ";
+            symbol = " ";
         };
 
         package = {
-          symbol = " ";
+            symbol = " ";
 
         };
 
         python = {
-          symbol = " ";
+            symbol = " ";
+            format = "[ $symbol($version(-$name) )]($style)";
+        };
+
+        os = {
+            format = "[ $symbol]($style)";
+            style = "fg:#D8DEE9";
+            symbols = {
+                NixOS = " ";
+                Ubuntu = " ";
+            };
+            disabled = false;
         };
 
 
+        # error status of executed command.
+        status = {
+            style = "fg:#FD0000";
+            format = "[ ✗ $int - $common_meaning]($style) ";
+            map_symbol = true;
+            disabled = false;
+        };
+
+        };
       };
-    };
 }
