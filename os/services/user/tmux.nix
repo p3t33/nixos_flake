6f0a3582bbaa9f4130dfd1116b7b043b96b54e97
Kobi Medrish <kobi.medrish@gmail.com>
2023-10-19 16:21:15 +0300

    Updated tmux systemd service
    - Made systemd service to restart the service if it crashes.
    - Did some cleanup and added comments.

--- os/services/user/tmux.nix
+++ os/services/user/tmux.nix
@@ -7,15 +7,16 @@
     description = "tmux server";
 
     # creates the [Service] section
+    # based on the emacs systemd service
+    # does not source uses a login shell so does not load ~/.zshrc in case this is needed
+    # just add -l(E.g bash -cl "...").
     serviceConfig = {
       Type = "forking";
-      #Environment = "PATH=/run/wrappers/bin:/home/${user}/.nix-profile/bin:/etc/profiles/per-user/${user}/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin";
-      #ExecStart="${pkgs.tmux}/bin/tmux new-session -d";
+      Restart = "always";
       ExecStart="${pkgs.bash}/bin/bash -c 'source ${config.system.build.setEnvironment} ; exec ${pkgs.tmux}/bin/tmux start-server'";
       ExecStop="${pkgs.tmux}/bin/tmux kill-server";
     };
 
     wantedBy = [ "default.target" ];
-
   };
 }
