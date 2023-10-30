3a9fd479545c6e983d22b9a55df92f3013875d03
Kobi Medrish <kobi.medrish@gmail.com>
2023-10-30 14:03:26 +0200

    Refactored flake.nix
    I have removed some code that were reused(nixosConfigurations) and
    more importantly made the flake ready to be used with a stand alone
    installation of home-manger, which is useful in case where NixOS is not
    an option.
    
    This is an initial step and mostly makes sure that the $HOME flake
    defined settings can be evaluated and applied using a stand alone home-manger
    installation. This does not yet cover 1:1 the experience that a user will get
    using the flake with NixOS.

--- flake.nix
+++ flake.nix
@@ -1,92 +1,92 @@
 {
-  description = "A very basic flake";
+    description = "Multi machine flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
-      inputs.nixpkgs.follows = "nixpkgs";
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+        home-manager = {
+            url = "github:nix-community/home-manager/release-23.05";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
-  };
 
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }:
+    outputs = inputs@{ nixpkgs, home-manager, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-	config.allowUnfree = true;
-      };
+        system = "x86_64-linux";
+        pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+        };
 
-      lib = nixpkgs.lib;
-    in{
-      nixosConfigurations = {
-        vm_server = lib.nixosSystem {
-	  inherit system;
-	  modules = [
-	  ./hosts/vm_server/configuration.nix
-	  home-manager.nixosModules.home-manager
-	  {
-	    home-manager.useGlobalPkgs = true;
-	    home-manager.useUserPackages = true;
-	    home-manager.users.drone = import ./hosts/vm_server/home.nix;
-	  }
-	  ];
-	};
-      };
+    lib = nixpkgs.lib;
+    in
+    {
+        nixosConfigurations = {
+            vm_server = lib.nixosSystem {
+                inherit system;
+                modules = [
+                    ./hosts/vm_server/configuration.nix
+                        home-manager.nixosModules.home-manager
+                        {
+                            home-manager.useGlobalPkgs = true;
+                            home-manager.useUserPackages = true;
+                            home-manager.users.drone = import ./hosts/vm_server/home.nix;
+                        }
+                ];
+            };
 
-      nixosConfigurations = {
-        vm_gui = lib.nixosSystem {
-	  inherit system;
-	  modules = [
-	  ./hosts/vm_gui/configuration.nix
-	  home-manager.nixosModules.home-manager
-	  {
-	    home-manager.useGlobalPkgs = true;
-	    home-manager.useUserPackages = true;
-	    home-manager.users.kmedrish = import ./hosts/vm_gui/home.nix;
-	  }
-	  ];
-	};
-      };
+            vm_gui = lib.nixosSystem {
+                inherit system;
+                modules = [
+                    ./hosts/vm_gui/configuration.nix
+                        home-manager.nixosModules.home-manager
+                        {
+                            home-manager.useGlobalPkgs = true;
+                            home-manager.useUserPackages = true;
+                            home-manager.users.kmedrish = import ./hosts/vm_gui/home.nix;
+                        }
+                ];
+            };
 
-      nixosConfigurations = {
-        work_pc = lib.nixosSystem {
-	  inherit system;
-	  modules = [
-	  ./hosts/work_pc/configuration.nix
-	  home-manager.nixosModules.home-manager
-	  {
-	    home-manager.useGlobalPkgs = true;
-	    home-manager.useUserPackages = true;
-	    home-manager.users.kmedrish = import ./hosts/work_pc/home.nix;
-	  }
-	  ];
-	};
-      };
+            work_pc = lib.nixosSystem {
+                inherit system;
+                modules = [
+                    ./hosts/work_pc/configuration.nix
+                        home-manager.nixosModules.home-manager
+                        {
+                            home-manager.useGlobalPkgs = true;
+                            home-manager.useUserPackages = true;
+                            home-manager.users.kmedrish = import ./hosts/work_pc/home.nix;
+                        }
+                ];
+            };
 
-      nixosConfigurations = {
-        home_desktop = lib.nixosSystem {
-	  inherit system;
-	  modules = [
-	  ./hosts/home_desktop/configuration.nix
-	  home-manager.nixosModules.home-manager
-	  {
-	    home-manager.useGlobalPkgs = true;
-	    home-manager.useUserPackages = true;
-	    home-manager.users.kmedrish = import ./hosts/home_desktop/home.nix;
-	  }
-	  ];
-	};
-      };
+            home_desktop = lib.nixosSystem {
+                inherit system;
+                modules = [
+                    ./hosts/home_desktop/configuration.nix
+                        home-manager.nixosModules.home-manager
+                        {
+                            home-manager.useGlobalPkgs = true;
+                            home-manager.useUserPackages = true;
+                            home-manager.users.kmedrish = import ./hosts/home_desktop/home.nix;
+                        }
+                ];
+            };
+        };
 
-    hmConfig = {
-      drone = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-	modules = [
-	  ./home/vm/home.nix
-	];
-      };
-    };
+        # Created for the case I would like to apply home manger settings on non NixOS
+        # OS such as Lubuntu.
+        # For this to work:
+        # 1. The host name on the existing system needs to match the one on ./hosts/work_pc/home.nix and ./meta/meta.nix
+        # 2. The username on the existing system most match the one defined in homeConfigurations.
+        homeConfigurations = {
+            kmedrish = home-manager.lib.homeManagerConfiguration {
+                inherit pkgs;
+                modules = [
+                    ./hosts/work_pc/home.nix
+                ];
+            };
+        };
     };
 }
