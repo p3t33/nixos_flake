5ca54e0acd5470cd26e8bd9aa58e79e98b874169
Kobi Medrish <kobi.medrish@gmail.com>
2023-11-05 22:08:55 +0200

    Updated nvim to use lualine instead of airline
    I was having some issues to setup airline so I decided
    for the time being to switch to lualine instead as my
    status bar.

--- home/modules/neovim/neovim.nix
+++ home/modules/neovim/neovim.nix
@@ -446,14 +446,49 @@ in
       # Status/tabline
       # --------------
       {
-          plugin = vim-airline;
+          plugin = lualine-nvim;
+          type = "lua";
           config = ''
-
-              " Show just the filename
-              let g:airline_section_c = '%t'
-              "without puting this setting to 0 I was unable to disable section y"
-              let g:webdevicons_enable_airline_statusline_fileformat_symbols = 0
-              let g:airline_section_y = ""
+            require('lualine').setup {
+                options = {
+                    icons_enabled = true,
+                    theme = 'nord',
+                    component_separators = { left = '', right = ''},
+                    section_separators = { left = '', right = ''},
+                    disabled_filetypes = {
+                        statusline = {},
+                        winbar = {},
+                    },
+                    ignore_focus = {},
+                    always_divide_middle = true,
+                    globalstatus = false,
+                    refresh = {
+                        statusline = 1000,
+                        tabline = 1000,
+                        winbar = 1000,
+                    }
+                },
+                sections = {
+                    lualine_a = {'mode'},
+                    lualine_b = {'branch', 'diff', 'diagnostics'},
+                    lualine_c = {'filename'},
+                    lualine_x = {'filetype'},
+                    lualine_y = {'progress'},
+                    lualine_z = {'location'}
+                },
+                inactive_sections = {
+                    lualine_a = {},
+                    lualine_b = {},
+                    lualine_c = {'filename'},
+                    lualine_x = {'location'},
+                    lualine_y = {},
+                    lualine_z = {}
+                },
+                tabline = {},
+                winbar = {},
+                inactive_winbar = {},
+                extensions = {}
+            }
           '';
       }
 
