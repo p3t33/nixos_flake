4ab8e2d5008827cb46ffad106c0c0fb87fbb4e5d
Kobi Medrish <kobi.medrish@motorolasolutions.com>
2023-12-18 09:44:24 +0200

    Added a new host to the falke with disko support - Added a new host that currently runs inside of VM until it is flashed   out. - Added disko as flake input. - Setup up the host to use the disko input for disk partitions.   The disko settings used to partition the hard drive during OS     installation and then used to setup /etc/fstab for boot loader and nixos     to use.

---
+++ hosts/homelab/configuration-services.nix
@@ -0,0 +1,11 @@
+{ ... }:
+{
+
+  imports = [
+    ../../modules/nixos/services/sshd.nix
+    ../../modules/nixos/services/user/tmux.nix
+    ../../modules/nixos/services/system/moolticuted.nix
+    ../../modules/nixos/services/user/watchman.nix
+  ];
+
+}
---
+++ hosts/homelab/home.nix
@@ -0,0 +1,57 @@
+{ config, pkgs, ... }:
+
+{
+
+  # IMPORTANT: hostname must be defined!
+  # All of the global variables are defined based on the value set for it. Many
+  # files use them and by not setting the hostname they will be using thier
+  # default values which may cause all kind of issues.
+  userDefinedGlobalVariables = {
+      hostname = "homelab";
+  };
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = config.userDefinedGlobalVariables.username;
+  home.homeDirectory = config.userDefinedGlobalVariables.homeDirectory;
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = config.userDefinedGlobalVariables.homeManagerStateVersion;
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  imports = [
+    ../../meta/meta.nix
+    ../../modules/home-manager/starship.nix
+    ../../modules/home-manager/tmux.nix
+    ../../modules/home-manager/fzf.nix
+    ../../modules/home-manager/neovim/neovim.nix
+    ../../modules/home-manager/git.nix
+    ../../modules/home-manager/zoxide.nix
+    ../../modules/home-manager/zsh.nix
+    ../../modules/home-manager/bash.nix
+    ../../modules/home-manager/taskwarrior.nix
+    ../../modules/home-manager/scripts/tmux-sessionizer.nix
+    ../../modules/home-manager/scripts/cheat-sh.nix
+    ../../modules/home-manager/nix-index.nix
+    ../../modules/home-manager/atuin.nix
+    ../../modules/home-manager/lazygit.nix
+    ../../modules/home-manager/syncthing.nix
+    ../../modules/home-manager/gpg.nix
+    ../../modules/home-manager/bat.nix
+
+  ];
+
+  home.sessionVariables = {
+      EDITOR = config.userDefinedGlobalVariables.editor;
+      SUDO_EDITOR = config.userDefinedGlobalVariables.editor;
+      MANPAGER = config.userDefinedGlobalVariables.manPager;
+  };
+}
