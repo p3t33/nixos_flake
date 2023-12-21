84d40a7ec2a5edb745e05fc2476e774286c6b764
Kobi Medrish <kobi.medrish@motorolasolutions.com>
2023-12-21 09:11:41 +0200

    Created additional disko configurations for mount only hard drives
    As disko used both for formatting hard drives and then mounting them
    as part of the NixOS configuration. The hard drives that are only
    to be mounted need to be broken off into a separate file file for NixOS
    to import. If the separation is not done the disko "format" command
    will format them as well.

---
+++ hosts/homelab/disko-config-extra-hard-dirves.nix
@@ -0,0 +1,48 @@
+# disko configuration files serve a double purpose. They are used to format
+# the hard drives and to mount them as part of NixOS configurations.
+# This file represents the hard drives that are not to be formatted but only
+# mounted.
+#
+# disko will format everything it is presented with and so the disks that
+# are only meant to be mounted by NixOS must be separated in their own file.
+{
+    disko.devices = {
+        disk = {
+            sdb = {
+                device = "/dev/sdb";
+                type = "disk";
+                content = {
+                    type = "gpt";
+                    partitions = {
+                        media = {
+                            size = "100%";
+                            content = {
+                                type = "filesystem";
+                                format = "ext4";
+                                mountpoint = "/mnt/media";
+                            };
+                        };
+                    };
+                };
+            };
+
+            sdc = {
+                device = "/dev/sdc";
+                type = "disk";
+                content = {
+                    type = "gpt";
+                    partitions = {
+                        data = {
+                            size = "100%";
+                            content = {
+                                type = "filesystem";
+                                format = "ext4";
+                                mountpoint = "/mnt/data";
+                            };
+                        };
+                    };
+                };
+            };
+        };
+    };
+}
