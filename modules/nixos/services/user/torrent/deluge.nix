ec60285dd552ab5169d25961ffb6143772b0727c
Kobi Medrish <kobi.medrish@motorolasolutions.com>
2023-12-25 16:52:43 +0200

    Fixed deluge configurations

--- modules/nixos/services/user/torrent/deluge.nix
+++ modules/nixos/services/user/torrent/deluge.nix
@@ -14,9 +14,9 @@ services.deluge = {
         # /mnt/media is a mount point with defined owner, and permissions
         # so I was getting warning when NixOS was switching.
         download_location = "/mnt/media/torrents";
-        max_active_seeding = "200";
-        max_active_downloading = "200";
-        max_active_limit = "200";
+        max_active_seeding = 200;
+        max_active_downloading = 200;
+        max_active_limit = 200;
         allow_remote = false;
         dht = false;
         upnp = false;
