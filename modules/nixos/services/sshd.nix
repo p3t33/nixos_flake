aee8fb653f0dfbc2e0583d4de9db551706ff9e99
Kobi Medrish <kobi.medrish@gmail.com>
2023-12-28 11:43:27 +0200

    Basic sshd hardening

--- modules/nixos/services/sshd.nix
+++ modules/nixos/services/sshd.nix
@@ -1,5 +1,10 @@
 { ... }:
 {
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "no";
+    passwordAuthentication = false;
+    challengeResponseAuthentication = false;
+  };
 }
