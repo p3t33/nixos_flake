bae2a213d0e1cb068c91f21e58baf36e9ebc8c88
Kobi Medrish <kobi.medrish@gmail.com>
2024-01-02 13:59:16 +0200

    Updated the name of hostTag global option I defined.
    Changed the variable name for readability.

--- modules/nixos/auto_upgrade.nix
+++ modules/nixos/auto_upgrade.nix
@@ -2,7 +2,7 @@
 {
     system.autoUpgrade.enable = true;
     system.autoUpgrade.dates  = "Fri *-*-1..7,15..21 01:00:00";
-    system.autoUpgrade.flake  = "github:${config.userDefinedGlobalVariables.githubFlakeRepositoryName}#${config.userDefinedGlobalVariables.hostTag}";
+    system.autoUpgrade.flake  = "github:${config.userDefinedGlobalVariables.githubFlakeRepositoryName}#${config.userDefinedGlobalVariables.hostConfigurationName}";
     system.autoUpgrade.randomizedDelaySec = "5m";
 
 }
