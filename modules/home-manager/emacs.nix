a669bfca4697efa0a845cd2b00ed6b9aec45f846
Kobi Medrish <kobi.medrish@motorolasolutions.com>
2024-01-08 21:18:48 +0200

    init for fzf for emacs

--- modules/home-manager/emacs.nix
+++ modules/home-manager/emacs.nix
@@ -58,6 +58,7 @@
             epkgs.magit
             epkgs.hl-todo
             epkgs.tldr
+            epkgs.fzf
         ];
     };
 
@@ -106,6 +107,21 @@
             ;; for its own configuration needs to be loaded only after
             ;; this macro has been loaded.
             (require 'use-package)
+
+            (use-package fzf
+             :bind
+             ;; Don't forget to set keybinds!
+             :config
+             (setq fzf/args "-x --color bw --print-query --margin=1,0 --no-hscroll"
+              fzf/executable "fzf"
+              fzf/git-grep-args "-i --line-number %s"
+              ;; command used for `fzf-grep-*` functions
+              ;; example usage for ripgrep:
+              ;; fzf/grep-command "rg --no-heading -nH"
+              fzf/grep-command "grep -nrH"
+              ;; If nil, the fzf buffer will appear at the top of the window
+              fzf/position-bottom t
+              fzf/window-height 15))
 
             ;; Configure and initialize evil-mode
             ;; =================================
