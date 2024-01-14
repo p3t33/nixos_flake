0a75cd568ad0c4421cae85190c41e1d8fd4e7b6a
Kobi Medrish <kobi.medrish@gmail.com>
2024-01-14 16:06:16 +0200

    Removed fzf stand alone pligin from emacs

--- modules/home-manager/emacs.nix
+++ modules/home-manager/emacs.nix
@@ -59,7 +59,6 @@
             epkgs.magit
             epkgs.hl-todo
             epkgs.tldr
-            epkgs.fzf
             epkgs.org-roam
         ];
     };
@@ -155,22 +154,6 @@
             ;; for its own configuration needs to be loaded only after
             ;; this macro has been loaded.
             (require 'use-package)
-
-            (use-package fzf
-             :bind
-             ;; Don't forget to set keybinds!
-             :config
-             (setq fzf/args "-x --color bw --print-query --margin=1,0 --no-hscroll"
-              fzf/executable "fzf"
-              fzf/git-grep-args "-i --line-number %s"
-              ;; command used for `fzf-grep-*` functions
-              ;; example usage for ripgrep:
-              ;; fzf/grep-command "rg --no-heading -nH"
-              fzf/grep-command "grep -nrH"
-              ;; If nil, the fzf buffer will appear at the top of the window
-              fzf/position-bottom t
-              fzf/window-height 15))
-
 
             ;; Configure and initialize evil-mode
             ;; =================================
