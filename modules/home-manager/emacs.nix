e5e300277e9e2d4e1fbba173f419c0a0d9a05bda
Kobi Medrish <kobi.medrish@motorolasolutions.com>
2024-01-21 21:03:53 +0200

    Updated default encoding for emacs

--- modules/home-manager/emacs.nix
+++ modules/home-manager/emacs.nix
@@ -103,10 +103,11 @@
                            (message "org-mode hook executed")
                            (org-redisplay-inline-images)))
 
-
-
-
-
+            ;; Set UTF-8 as the default encoding
+            (prefer-coding-system 'utf-8)
+            (set-default-coding-systems 'utf-8)
+            (set-terminal-coding-system 'utf-8)
+            (set-keyboard-coding-system 'utf-8)
 
             ;; smoth scroll with mergin of lines
             ;; =================================
