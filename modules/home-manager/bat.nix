796438d33886e0b3ae5bc4114d199d9ac37b774e
Kobi Medrish <kobi.medrish@motorolasolutions.com>
2024-02-18 21:17:16 +0200

    Small tweak to bat

--- modules/home-manager/bat.nix
+++ modules/home-manager/bat.nix
@@ -1,6 +1,10 @@
 {
   programs.bat = {
     enable = true;
-    config.theme = "Visual Studio Dark+";
+    config = {
+        theme = "Visual Studio Dark+";
+        wrap = "character";
+        terminal-width = "80";
+    };
   };
 }
