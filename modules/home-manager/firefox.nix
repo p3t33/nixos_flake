a5ad8d90a702fa23d6de4002e8f771e60377c24b
Kobi Medrish <kobi.medrish@gmail.com>
2024-03-03 17:26:17 +0200

    Firefox plugins overhaul
    firefox plugins are now declaratively defined without the use of
    firefox-addons flake input to NUR.
    
    - Removed firefox-addons input from flake.
    - Replaced deprecated firefox vim-vixen plugin with vimium.
    - Added firenvim plugin. This plugin allows to embed neovim into
    text objects inside of firefox.
    - Added tree-style-tab plugin.
    - Added privacy-badger plugin.
    - Added mooltipass plugin.
    - Added yet another speed dial plugin.

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -1,7 +1,34 @@
-{config, pkgs, inputs, ...}:
+{config, pkgs, ...}:
 {
     programs.firefox = {
         enable = true;
+        policies = {
+            ExtensionSettings = with builtins;
+            let extension = shortId: uuid: {
+              name = uuid;
+              value = {
+                install_url = "https://addons.mozilla.org/firefox/downloads/latest/${shortId}/latest.xpi";
+                installation_mode = "normal_installed";
+              };
+            };
+            # They way to get uuid is to install the plugin in a regular manner the plugin you would like to declare
+            # and then from the firefox url execute about:debugging#/runtime/this-firefox
+            # copy the uuid and uninstal the plugin.
+            #
+            # to get the shortId value you will need to do some trial and error and see that the url is valild and when used from
+            # firefox url it will trigger installtion.
+            # In most cases if the name of the plugin is more then one word you will use - between them.
+            # Its best to go the the page of the addond and copy its url and from there to try and find out its shortId.
+            in listToAttrs [
+              (extension "ublock-origin" "uBlock0@raymondhill.net")
+              (extension "privacy-badger17" "jid1-MnnxcxisBPnSXQ@jetpack")
+              (extension "mooltipass-extension" "{da36041e-5ce1-4c8a-9dc5-406cc98f0a23}")
+              (extension "yet-another-speed-dial" "yet_another_speed_dial@conceptualspace.net")
+              (extension "tree-style-tab" "treestyletab@piro.sakura.ne.jp")
+              (extension "vimium-ff" "{d7742d87-e61d-4b78-b8a1-b469842139fa}")
+              (extension "hover-zoom-plus" "{92e6fe1c-6e1d-44e1-8bc6-d309e59406af}")
+            ];
+        };
 
         profiles.${config.userDefinedGlobalVariables.username} = {
             # to verify inter into the search bar about:profiles
@@ -81,15 +108,6 @@
                     url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
                 }
             ];
-
-            extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
-                ublock-origin
-                hover-zoom-plus
-                vim-vixen
-
-            ];
         };
-
-
     };
 }
