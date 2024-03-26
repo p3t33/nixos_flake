ffb360d0321800ee3bce746ded7cd4b5d70405c5
Kobi Medrish <kobi.medrish@gmail.com>
2024-03-26 13:19:39 +0200

    Refined nitrokey configurations
    - Added gui management app.
    - Added dependencies for the key to communicate with the OS.

--- modules/nixos/security/nitrokey.nix
+++ modules/nixos/security/nitrokey.nix
@@ -1,7 +1,12 @@
 { pkgs, ... }:
 {
     environment.systemPackages = with pkgs; [
-        pynitrokey
+        pynitrokey # cli for nk3
+        nitrokey-app2 # gui for nk3
+
+        # FIDO/FIDO(U2F) libraries dependencies.
+        libfido2 # webauto
+        pam_u2f #linux(sudo, display manager, console login...)
     ];
 
     services.udev.packages = [ pkgs.nitrokey-udev-rules];
