f8238ccd5b6dd54feca726b7e5e55322b1a3095e
Kobi Medrish <kobi.medrish@motorolasolutions.com>
2024-05-02 12:33:12 +0300

    Cleaned up xserver settings
    - Made it more generic.
    - Made it more readable.
    - Removed cinnamon desktop environment.

--- modules/nixos/services/xserver.nix
+++ modules/nixos/services/xserver.nix
@@ -2,47 +2,46 @@
 
 {
 
-  services.xserver = {
-    enable = true;
-    logFile = "/var/log/Xorg.0.log"; # Enables logging to this file
-    layout = "us,il";
-    xkbVariant = "";
-    xkbOptions = "grp:win_space_toggle";
-
-    displayManager = {
-      lightdm.enable = true;
-      defaultSession = "none+i3";
-      autoLogin = {
+    services.xserver = {
         enable = true;
-        user = "kmedrish";
-      };
+        logFile = "/var/log/Xorg.0.log"; # Enables logging to this file
+        layout = "us,il";
+        xkbVariant = "";
+        xkbOptions = "grp:win_space_toggle";
+
+        displayManager = {
+            lightdm.enable = true;
+            defaultSession = "none+i3";
+            autoLogin = {
+                enable = true;
+                user = config.userDefinedGlobalVariables.username;
+            };
+        };
+
+        desktopManager = {
+            xterm.enable = false;
+        };
+
+        windowManager.i3 = {
+            enable = true;
+            #configFile = "/etc/i3.conf";
+            extraPackages = with pkgs; [
+                rofi
+                i3status
+                i3blocks
+
+                # Can query the window manager for information and is dependency for
+                # the rofi Firefox bookmars script.
+                wmctrl
+
+                # Monitor control
+                arandr
+                xorg.xrandr
+
+                # debugging and information
+                xorg.xdpyinfo
+                glxinfo # query the properties of an OpenGL implementation
+            ];
+        };
     };
-
-    desktopManager = {
-      cinnamon.enable = true;
-    };
-
-    windowManager.i3 = {
-      enable = true;
-      #configFile = "/etc/i3.conf";
-      extraPackages = with pkgs; [
-        rofi
-        i3status
-        i3blocks
-
-        # Can query the window manager for information and is dependency for
-        # the rofi Firefox bookmars script.
-        wmctrl
-
-        # Monitor control
-        arandr
-        xorg.xrandr
-
-        # debugging and information
-        xorg.xdpyinfo
-        glxinfo # query the properties of an OpenGL implementation
-      ];
-    };
-  };
-
 }
