ac1ac81ad668de1c585de64b52c76c74250ca5b7
Kobi Medrish <kobi.medrish@gmail.com>
2024-05-19 16:49:47 +0300

    Updated sops for home_desktop host
    Added ssh keys for personal development VMs.

--- hosts/home_desktop/sops-configuration.nix
+++ hosts/home_desktop/sops-configuration.nix
@@ -5,5 +5,6 @@
         ../../modules/nixos/security/sops/sops-syncthing.nix
         ../../modules/nixos/security/sops/sops-extra-ssh-hosts.nix
         ../../modules/nixos/security/sops/sops-git-credentials.nix
+        ../../modules/nixos/security/sops/ssh_development_keys_for_vm.nix
     ];
 }
--- modules/nixos/security/sops/ssh_development_keys_for_vm.nix
+++ modules/nixos/security/sops/ssh_development_keys_for_vm.nix
@@ -1,12 +1,12 @@
 { config, ... }:
 {
-    sops.secrets.ssh_id_ed25519_development_vm_private_key = {
+    sops.secrets."development_vm_ssh_keys/private" = {
         mode = "0600";
         owner = "${config.userDefinedGlobalVariables.primeUsername}";
         path = "${config.userDefinedGlobalVariables.primeUserHomeDirectory}/.ssh/id_ed25519_development_vm";
     };
 
-    sops.secrets.ssh_id_ed25519_development_vm_public_key = {
+    sops.secrets."development_vm_ssh_keys/public" = {
         mode = "0600";
         owner = "${config.userDefinedGlobalVariables.primeUsername}";
         path = "${config.userDefinedGlobalVariables.primeUserHomeDirectory}/.ssh/id_ed25519_development_vm.pub";
