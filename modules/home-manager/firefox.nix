e1bc4b0f8cd826288871cee4a6935ba175205558
Kobi Medrish <kobi.medrish@motorolasolutions.com>
2024-06-02 17:22:12 +0300

    Updated session management and keybinding in firefox
    - Added plugins to manage sessions.
    - Added a plugin to set keybindings.
    - Firefox will now restore a session if it has one without the user
      being required to do so manually.

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -13,12 +13,23 @@
             };
             # They way to get uuid is to install the plugin in a regular manner the plugin you would like to declare
             # and then from the firefox url execute about:debugging#/runtime/this-firefox
-            # copy the uuid and uninstal the plugin.
+            # copy the Extention ID(and not the internal UUID) and uninstal the plugin.
             #
-            # to get the shortId value you will need to do some trial and error and see that the url is valild and when used from
-            # firefox url it will trigger installtion.
+            # to get the  uuid/name value you will need to do some trial and error and see that the url is valild and when used from
+            # firefox url it will trigger installtion(replace ${shortid} with
+            # the uuid in the url address..
             # In most cases if the name of the plugin is more then one word you will use - between them.
             # Its best to go the the page of the addond and copy its url and from there to try and find out its shortId.
+            #
+            # shortId that are email do not go into curly brackets. while the
+            # hash looking shortId do. Failing to do so will prevent the
+            # installtion of the plugin.
+            #
+            # it is important to undertand that failng to provide a valid
+            # shorId(with or wihoht the curly brackets) or uuid/name will not
+            # fail the build of homw manger but you won't get the plugin inside
+            # of firefox one you restart it.
+
             in listToAttrs [
               (extension "ublock-origin" "uBlock0@raymondhill.net")
               (extension "privacy-badger17" "jid1-MnnxcxisBPnSXQ@jetpack")
@@ -27,6 +38,9 @@
               (extension "tree-style-tab" "treestyletab@piro.sakura.ne.jp")
               (extension "vimium-ff" "{d7742d87-e61d-4b78-b8a1-b469842139fa}")
               (extension "hover-zoom-plus" "{92e6fe1c-6e1d-44e1-8bc6-d309e59406af}")
+              (extension "shortkeys" "Shortkeys@Shortkeys.com")
+              (extension "simple-tab-groups" "simple-tab-groups@drive4ik") # conflicts with "Tab Session Manager"
+              (extension "session-boss" "sessionboss@william.wong")
             ];
         };
 
@@ -38,6 +52,7 @@
             # The list bellow reflects the configuration that are available from
             # the search bar using about:config
             settings = {
+                "browser.startup.page" = 3; # This line is added to automatically restore the session
                 # Disable Pocket Integration
                 "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
                 "extensions.pocket.enabled" = false;
