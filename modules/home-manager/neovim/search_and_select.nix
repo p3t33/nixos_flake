bb9170cfd4023fb9ee62fd3a337c82d1892f684b
Kobi Medrish <kobi.medrish@gmail.com>
2024-06-02 16:49:32 +0300

    Updated neovim plugin harpoon to harpoon2

--- modules/home-manager/neovim/search_and_select.nix
+++ modules/home-manager/neovim/search_and_select.nix
@@ -171,19 +171,21 @@
 
       {
         # specify "hooks" on files and then manage them.
-        plugin = harpoon;
+        plugin = harpoon2;
         type = "lua";
         config = ''
-          local mark = require("harpoon.mark")
-          local ui = require("harpoon.ui")
+            local harpoon = require("harpoon")
+            -- REQUIRED
+            harpoon:setup()
+            -- REQUIRED
 
-          vim.keymap.set("n", "<leader>a", mark.add_file)
-          vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-          vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
-          vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
-          vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
-          vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
+            vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
         '';
       }
 
