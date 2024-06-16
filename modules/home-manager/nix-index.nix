73b33c168562292279c0045664abc4e0152d2f4d
Kobi Medrish <kobi.medrish@gmail.com>
2024-06-16 10:09:49 +0300

    Temporary fix for nix-index
    nix-index is used for command-not-found when using flake.
    I was using an option where I used pre compiled index as a file inside
    of my home directory. This option got broken in the latest nix-index
    update. For the time being there is a need to execute nix-index from the
    cli for the index to work.
    
    I will look into the recompiled index file option if it is still
    possible.

--- modules/home-manager/nix-index.nix
+++ modules/home-manager/nix-index.nix
@@ -1,12 +1,19 @@
 { inputs, pkgs, ... }:
 {
-    home.file = {
-# Put the pre-generated nix-index database in place,
-# used for command-not-found.
-        ".cache/nix-index/files".source =
-            inputs.nix-index-database.legacyPackages.${pkgs.system}.database;
-    };
 
+    # For this to work you will need to execute nix-index from the cli.
+    # In the past I was using:
+
+    # home.file = {
+    # Put the pre-generated nix-index database in place,
+    # used for command-not-found.
+    #    ".cache/nix-index/files".source =
+    #        inputs.nix-index-database.legacyPackages.${pkgs.system}.database;
+    #};
+
+
+    # but database is no longer a valid variable and I need some time to go
+    # over the changes in nix-index input to see how to fix it, if it even possible.
     programs.nix-index =
     {
         enable = true;
