3ce36d5982abc37d61081f3557d803c2976cdec7
Kobi Medrish <kobi.medrish@gmail.com>
2024-07-08 11:39:00 +0300

    Updated taskwarrior to version 3

--- modules/home-manager/taskwarrior.nix
+++ modules/home-manager/taskwarrior.nix
@@ -1,14 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
 {
     programs.taskwarrior = {
         enable = true;
+        package = pkgs.taskwarrior3;
 
-        # Will be used to sync data across devices  
+        # Will be used to sync data across devices
         dataLocation = "$HOME/Sync/taskwarrior_data/task";
 
         config = {
             weekstart="Sunday";
             confirmation = true;
+            dateformat = "D-M-Y";
+            news.version="3.0.2"; # this settings supress the random news message.
 
             alias."@" = "context";
             context."work" = "project:work";
