147be452b42ccff7423c1ef85914803d5b7db0d0
Kobi Medrish <kobi.medrish@gmail.com>
2024-07-08 11:26:50 +0300

    Updated garbage collection to include store optimization

--- modules/nixos/garbage_collection.nix
+++ modules/nixos/garbage_collection.nix
@@ -1,8 +1,15 @@
 { pkgs,  ... }:
 {
+    # systemcli status nix-gc.service
     nix.gc = {
         automatic = true;
         dates = "03:05";
-        options = "--delete-older-than 30d";
+        options = "--delete-older-than 5d";
+    };
+
+    # systemcli status nix-optimise.service
+    nix.optimise = {
+        automatic = true;
+        dates = [ "03:20"];
     };
 }
