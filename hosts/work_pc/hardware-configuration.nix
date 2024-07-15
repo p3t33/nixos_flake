97bfbd0c5f46e766c9eef56bda75321cba8d617f
Kobi Medrish <kobi.medrish@motorolasolutions.com>
2024-07-15 16:51:34 +0300

    Added support for intel hardware decoding

--- hosts/work_pc/hardware-configuration.nix
+++ hosts/work_pc/hardware-configuration.nix
@@ -9,6 +9,10 @@
         (modulesPath + "/installer/scan/not-detected.nix")
         ../../modules/nixos/gpu/nvidia.nix
         ../../modules/nixos/gpu/nvidia_hybrid_with_intel_offload_mode.nix
+         # I am using interl hardware decoding as I am not sure my nvidia chip supports it, prime in offload mode is not working
+         # for both VA-API and VDPAU , and I have not tried to use prime in sync mode. As at the moment I don't need
+         # hardware decoding I am fine with using the intel gpu settings which work just fine.
+        ../../modules/nixos/gpu/intel_hardware_decoding.nix
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "usbhid" "sd_mod" ];
