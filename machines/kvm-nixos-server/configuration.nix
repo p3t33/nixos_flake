5795118fbbd97951406067d1cccfe2954f086378
Kobi Medrish <kobi.medrish@gmail.com>
2024-12-01 21:34:06 +0200

    Updated kvm-nixos-server machine configuration with new keys

--- machines/kvm-nixos-server/configuration.nix
+++ machines/kvm-nixos-server/configuration.nix
@@ -58,7 +58,7 @@
     # By default will create /etc/ssh/authorized_keys.d/$USER file with this key in it.
     # This key is added for passwordless login and this key is for VM only
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINs6NNbZ6EaU1x7cem1zqhDYubadH5Uww+K28e6GOmiY Motorola no password"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHhh9jOBY5b+mv4CZqO8qr70RzpMHmjKy3P6kS9lP9KS used with virtual machines"
     ];
   };
 
