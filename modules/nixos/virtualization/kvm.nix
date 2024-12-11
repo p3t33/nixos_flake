68745dc26238c17b7eb2839011fdc39b5b01c42e
Kobi Medrish <kobi.medrish@gmail.com>
2024-12-11 10:30:32 +0200

    Added support of virtiofs for qemu/kvm

--- modules/nixos/virtualization/kvm.nix
+++ modules/nixos/virtualization/kvm.nix
@@ -1,10 +1,16 @@
 { pkgs, config, ... }:
 {
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    # requried for shared folders with virtiofs
+    qemu.vhostUserPackages = with pkgs; [ virtiofsd ];
+  };
+
   virtualisation.spiceUSBRedirection.enable = true;
   users.users.${config.userDefinedGlobalVariables.primeUsername} = {
     extraGroups = [ "libvirtd" ];
   };
+
 
   environment.systemPackages = with pkgs; [
     virt-manager
