49f32a817925045877fa5f7ec999fb778334db27
Kobi Medrish <kobi.medrish@gmail.com>
2024-12-24 12:25:27 +0200

    Updated git configuration file - Explicitly defined which editor to use for git commits. - Defined rebase to be use on all pull actions unless explicitly   configured otherwise. - New branches will now auto link to remote branche representation   if one exist(E.g foo to origin/foo). - Added some commetns.

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -26,21 +26,51 @@
 
     extraConfig = {
 
-      # This directive will rebase local chagnes on top of the remote branch
-      # in case branches diverged instead of the defualt of creating a commit
-      # on the local branch which will diverge from the remote branch.
-      # using rebase is a cleaner solution and will keep the history linear.
+      core = {
+        editor = "${config.userDefinedGlobalVariables.editor}";
+      };
+
+      pull = {
+        # This is the default for branches that has no expicit confiugraions,
+        # like the default one I set with autosetuprebase.
+        rebase = true;
+      };
+
+      push = {
+        # This directive makes git to apply force only if the local branch that
+        # is tracking the remote(E.g origin/master) hasn"t diverged from the remote"
+        # meaning in case that no new commits were added to the remote master.
+        # It avoides overwriting someone else work on the remote.
+        #
+        # implicitly makes "git push -f" into "git push --force-with-leas"
+        # to explicitly use "git pushh -f" you will need to execute "git push --force".
+        forceWithLease = true;
+      };
+
       branch = {
+        # This directive will rebase local chagnes on top of the remote branch
+        # in case branches diverged instead of the defualt of creating a commit
+        # on the local branch which will diverge from the remote branch.
+        # using rebase is a cleaner solution and will keep the history linear.
+        #
+        # This will only be applied on branches that are created after this
+        # config was added.
         autosetuprebase = "always";
+
+        # when creating a new branch(foo), will automatically linking it
+        # to thier correspondig remote branches(E.g origin/foo) if they exist
+        # on the remote so you can immediately use commands like git pull/push
+        # wihtout any additional setup.
+        autosetupmerge = "always";
       };
 
       merge = {
         conflictStyle = "zdiff3";
       };
-      # User and email defined in this file that is generated using sops
-      # For some reason git config --global user.[name/email] shows empty but
-      # executing git config user.[name/email] inside of a repo works just fine.
       include = {
+        # User and email defined in this file that is generated using sops
+        # For some reason git config --global user.[name/email] shows empty but
+        # executing git config user.[name/email] inside of a repo works just fine.
         path = config.sops.secrets.git.path;
       };
     };
