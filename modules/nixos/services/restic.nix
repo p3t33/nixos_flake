4ddd6e22878fd48c738104bbd4c3510288ad0a71
Kobi Medrish <kobi.medrish@gmail.com>
2025-02-25 10:42:29 +0200

    Added amazon remote to restic

--- modules/nixos/services/restic.nix
+++ modules/nixos/services/restic.nix
@@ -12,6 +12,9 @@ in
   sops.secrets."restic/gdrive/repositoryPathFile" = { };
   sops.secrets."restic/gdrive/passwordFile" = { };
   sops.secrets."restic/gdrive/rcloneConfigFile" = { };
+  sops.secrets."restic/amazon/repositoryPathFile" = { };
+  sops.secrets."restic/amazon/passwordFile" = { };
+  sops.secrets."restic/amazon/rcloneConfigFile" = { };
 
   services.restic.backups = {
     # name of restic repository, should be descriptive but could be anything.
@@ -39,7 +42,7 @@ in
         # The exact start time within this window will vary each day
         # due to the randomized delay.
         OnCalendar = "00:05";
-        RandomizedDelaySec = "2h";
+        RandomizedDelaySec = "1h";
       };
     };
 
@@ -59,10 +62,33 @@ in
       extraBackupArgs = [ compressionMax ];
 
       timerConfig = {
-          OnCalendar = "04:00";
-          RandomizedDelaySec = "2h";
+          OnCalendar = "02:05";
+          RandomizedDelaySec = "1h";
       };
 
+    };
+
+    amazon = {
+      initialize = true;
+
+      repositoryFile = config.sops.secrets."restic/amazon/repositoryPathFile".path;
+      rcloneConfigFile = config.sops.secrets."restic/amazon/rcloneConfigFile".path;
+      passwordFile = config.sops.secrets."restic/amazon/passwordFile".path;
+
+      paths = [ "${config.userDefinedGlobalVariables.pathToDataDirectory}/Sync" ];
+
+      pruneOpts = [
+        pruneDaily
+        pruneWeekly
+        pruneYearly
+      ];
+
+      extraBackupArgs = [ compressionMax ];
+
+      timerConfig = {
+        OnCalendar = "04:05";
+        RandomizedDelaySec = "1h";
+      };
     };
   };
 
