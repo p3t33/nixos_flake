3158b0ed5aaee4c6dfc5e5236bbca553c2205cc6
Kobi Medrish <kobi.medrish@gmail.com>
2025-04-15 22:50:06 +0300

    Updated default gui application - Set default for pdf - Refactored the config file to use a variable instead of repeating string.

--- modules/home-manager/xdg/mime_apps.nix
+++ modules/home-manager/xdg/mime_apps.nix
@@ -1,36 +1,41 @@
-{ config, pkgs, ... }:
-
-# Used to define defaults for the GUI based on file suffix.
+let
+  defaultWebBrowser = "firefox.desktop";
+  defaultPdfReader = "org.pwmt.zathura.desktop";
+in
 {
+  # Used to define defaults for the GUI based on file suffix.
+  # test E.g xdg-mime query default application/pdf
   xdg.mimeApps = {
     enable = true;
+
     associations.added = {
-      "x-scheme-handler/http" = [ "firefox.desktop" ];
-      "x-scheme-handler/https" = [ "firefox.desktop" ];
-      "x-scheme-handler/chrome" = [ "firefox.desktop" ];
-      "text/html" = [ "firefox.desktop" ];
-      "application/x-extension-htm" = [ "firefox.desktop" ];
-      "application/x-extension-html" = [ "firefox.desktop" ];
-      "application/x-extension-shtml" = [ "firefox.desktop" ];
-      "application/xhtml+xml" = [ "firefox.desktop" ];
-      "application/x-extension-xhtml" = [ "firefox.desktop" ];
-      "application/x-extension-xht" = [ "firefox.desktop" ];
-      "application/pdf" = [ "xreader.desktop" ];
+      "x-scheme-handler/http"  = [ defaultWebBrowser ];
+      "x-scheme-handler/https" = [ defaultWebBrowser ];
+      "x-scheme-handler/chrome" = [ defaultWebBrowser ];
+      "text/html" = [ defaultWebBrowser ];
+      "application/x-extension-htm" = [ defaultWebBrowser ];
+      "application/x-extension-html" = [ defaultWebBrowser ];
+      "application/x-extension-shtml" = [ defaultWebBrowser ];
+      "application/xhtml+xml" = [ defaultWebBrowser ];
+      "application/x-extension-xhtml" = [ defaultWebBrowser ];
+      "application/x-extension-xht" = [ defaultWebBrowser ];
+      "application/pdf" = [ defaultPdfReader ];
     };
+
     defaultApplications = {
-      "text/html" = [ "firefox.desktop" ];
-      "x-scheme-handler/http" = [ "firefox.desktop" ];
-      "x-scheme-handler/https" = [ "firefox.desktop" ];
-      "x-scheme-handler/about" = [ "firefox.desktop" ];
-      "x-scheme-handler/unknown" = [ "firefox.desktop" ];
-      "x-scheme-handler/chrome" = [ "firefox.desktop" ];
-      "application/x-extension-htm" = [ "firefox.desktop" ];
-      "application/x-extension-html" = [ "firefox.desktop" ];
-      "application/x-extension-shtml" = [ "firefox.desktop" ];
-      "application/xhtml+xml" = [ "firefox.desktop" ];
-      "application/x-extension-xhtml" = [ "firefox.desktop" ];
-      "application/x-extension-xht" = [ "firefox.desktop" ];
-      "application/pdf" = [ "xreader.desktop" ];
+      "text/html" = [ defaultWebBrowser ];
+      "x-scheme-handler/http" = [ defaultWebBrowser ];
+      "x-scheme-handler/https" = [ defaultWebBrowser ];
+      "x-scheme-handler/about" = [ defaultWebBrowser ];
+      "x-scheme-handler/unknown" = [ defaultWebBrowser ];
+      "x-scheme-handler/chrome" = [ defaultWebBrowser ];
+      "application/x-extension-htm" = [ defaultWebBrowser ];
+      "application/x-extension-html" = [ defaultWebBrowser ];
+      "application/x-extension-shtml" = [ defaultWebBrowser ];
+      "application/xhtml+xml" = [ defaultWebBrowser ];
+      "application/x-extension-xhtml" = [ defaultWebBrowser ];
+      "application/x-extension-xht" = [ defaultWebBrowser ];
+      "application/pdf" = [ defaultPdfReader ];
     };
   };
 }
