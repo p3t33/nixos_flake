6cbb01bbd5ab00be266b9737805bb4f8feec5a09
Kobi Medrish <kobi.medrish@gmail.com>
2025-04-24 19:00:06 +0300

    Updated gtk theme settings - Added thems and icons options - Defined nord for my gtk theme and and icons. - Defined gtk font size

--- modules/home-manager/gtk.nix
+++ modules/home-manager/gtk.nix
@@ -1,11 +1,33 @@
 { pkgs, config, ... }:
 {
-  home.packages = with pkgs; [ adwaita-icon-theme ];
+  home.packages = with pkgs; [
+    arc-theme
+    arc-icon-theme
+    papirus-icon-theme
+    tela-icon-theme
+  ];
 
   gtk = {
     enable = true;
-    font.name = config.userDefinedGlobalVariables.font.sansSerif;
-    iconTheme.name = "Adwaita";
-    theme.name = "Adwaita-dark";
+    font = {
+      name = config.userDefinedGlobalVariables.font.sansSerif;
+      size = 12;
+    };
+
+    theme = {
+      name = "Nordic-darker";
+      package = pkgs.nordic;
+    };
+
+    iconTheme = {
+      name = "Nordic-bluish";
+      package = pkgs.nordic;
+    };
+
+    cursorTheme = {
+      name = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
+    };
   };
+
 }
