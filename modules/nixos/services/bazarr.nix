6d8ae5ff5f9aa9a44ee3cb7a763490649ecc5a3f
Kobi Medrish <kobi.medrish@gmail.com>
2025-05-28 15:01:19 +0300

    Added widgets to homepage Dashboard
    - Added widgets to some of the services.
    - Secrets(tokens/passwords) are stored as environment variables in sops file
    that is passed as an environment file to the homepage-dashboard systemd unit.

--- modules/nixos/services/bazarr.nix
+++ modules/nixos/services/bazarr.nix
@@ -3,10 +3,9 @@ let
   serviceName = "bazarr";
 in
 {
-  # Enable the Sonarr service(as of now there is no config for default sonarr port)
   services.${serviceName} = {
     enable = true;
-    openFirewall = true; # Opens Sonarr's port on the firewall (default 8989)
+    openFirewall = true;
     user = "${serviceName}";
     group = "${config.userDefinedGlobalVariables.mediaGroup}";
     listenPort = 6767;
