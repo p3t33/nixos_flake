7264a29ec6334345fdf3db58aa7f188dc0d66b9d
Kobi Medrish <kobi.medrish@gmail.com>
2025-06-11 10:57:15 +0300

    Added theme to atuin

--- modules/home-manager/atuin.nix
+++ modules/home-manager/atuin.nix
@@ -1,5 +1,8 @@
 { ... }:
 
+let
+  catppuccinTheme = "catppuccin-mocha-blue";
+in
 {
   programs.atuin = {
     enable = true;
@@ -12,6 +15,26 @@
       style = "compact";
       # for some reason this directive has no effect.
       show_help = false;
+
+      theme = {
+        name = catppuccinTheme;
+      };
+    };
+
+    themes = {
+      "${catppuccinTheme}" = {
+        theme.name = catppuccinTheme;
+        colors = {
+          AlertInfo = "#a6e3a1";
+          AlertWarn = "#fab387";
+          AlertError = "#f38ba8";
+          Annotation = "#89b4fa";
+          Base = "#cdd6f4";
+          Guidance = "#9399b2";
+          Important = "#f38ba8";
+          Title = "#89b4fa";
+        };
+      };
     };
   };
 }
