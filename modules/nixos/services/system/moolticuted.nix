aa70f3cf3fb61a26d2c675a37451b9e5f6ea21e5
Kobi Medrish <kobi.medrish@gmail.com>
2025-06-12 10:11:39 +0300

    updated moolticuted service unit
    Went over the unit provided as part of the official repository and
    made a few small adjustments.

--- modules/nixos/services/system/moolticuted.nix
+++ modules/nixos/services/system/moolticuted.nix
@@ -1,17 +1,28 @@
 { pkgs, config, ... }:
 {
 
+  # Based on the systmed unit provided in the moolticute repository
+  #
+  # IMPORTANT: If this unit is used it is best to disable the option found
+  # in the GUI called "Start Moolticute with the computer".
+
   systemd.services.moolticuted = {
     enable = true;
     description = "Moolticute daemon";
 
     serviceConfig = {
       Type = "simple";
+      # handles some race conditions, without this sleep, service fails with:
+      # Failed to attach to shared segment:  "QSharedMemoryPrivate::initKey: unable to set key on lock"
       ExecStartPre = "${pkgs.coreutils}/bin/sleep 5";
       ExecStart = "${pkgs.moolticute}/bin/moolticuted";
       killMode = "process";
       Restart = "always";
       User = "${config.userDefinedGlobalVariables.primeUsername}";
+      # --- Why 'Group=' is Not Used ---
+      # No 'Group=' line is needed because the 'uaccess' tag already gives
+      # the user specified above all the necessary permissions. Adding a
+      # 'Group=' would be a redundant,
 
       CapabilityBoundingSet = "";
       RuntimeDirectory = "moolticuted";
@@ -29,8 +40,7 @@
       RestrictRealtime = true;
     };
 
-    after = [ "graphical.target" ];
-    wantedBy = [ "graphical.target" ];
+    wantedBy = [ "multi-user.target" ];
   };
 
   services.udev.packages = [ pkgs.moolticute.udev ];
