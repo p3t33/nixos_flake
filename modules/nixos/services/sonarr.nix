89e650716911f2ba3c420e5461dcba7d31279475
Kobi Medrish <kobi.medrish@gmail.com>
2025-07-29 19:08:03 +0300

    Removed the use of insecure packages for sonarr
    Looks like sonarr is moved away from using insecure packages
    so I have removed the promotion I created for them.

--- modules/nixos/services/sonarr.nix
+++ modules/nixos/services/sonarr.nix
@@ -24,14 +24,5 @@ in
         config.sops.secrets."${serviceName}/apiKey".path
       ];
     };
-
-    # sonarr is using end of life dotnet, which was marked as insecure in NixOS 24.11
-    # Untill soanrr moves to newer version this workaound is requried.
-    nixpkgs.config.permittedInsecurePackages = [
-      "aspnetcore-runtime-6.0.36"
-      "aspnetcore-runtime-wrapped-6.0.36"
-      "dotnet-sdk-6.0.428"
-      "dotnet-sdk-wrapped-6.0.428"
-    ];
   };
 }
