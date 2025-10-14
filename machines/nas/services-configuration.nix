96302f26365032cbe3abe6c9190474b5d6bbd2b6
Kobi Medrish <kobi.medrish@gmail.com>
2025-10-14 14:46:40 +0300

    Enabled syncthing
    Enabled syncthing for nas machine
    
    Created an instance of syncthing for the nas machine and defined it to
    be used with other machines on the network

--- machines/nas/services-configuration.nix
+++ machines/nas/services-configuration.nix
@@ -1,44 +1,110 @@
-{ ... }:
+{ config, lib, ... }:
 {
 
   imports = [
     ../../modules/nixos/services # imported via default.nix
   ];
 
-
-    services = {
-      zfs.autoScrub.enable = true;
-      nginx.enable = true;
-    # syncthing.enable = true;
-      adguardhome.enable = true;
-      homepage-dashboard.enable = true;
-    # calibre-web.enable = true;
-    # jellyfin.enable = true;
-      prowlarr.enable = true;
-    # jackett.enable = true;
-      sonarr.enable = true;
-      radarr.enable = true;
-    # readarr.enable = true;
-    # bazarr.enable = true;
-      deluge.enable = true;
-      sabnzbd.enable = true;
-    # inadyn.enable = true;
-      gatus.enable = true;
-      # prometheus.enable = true;
-      # promtail.enable = true;
-      # loki.enable = true;
-      # grafana.enable = true;
-    # samba.enable = true;
-    # paperless.enable = true;
-      postgresql.enable = true;
-      postgresqlBackup.enable = true;
+  services = {
+    zfs.autoScrub.enable = true;
+    nginx.enable = true;
+    syncthing.enable = true;
+    adguardhome.enable = true;
+    homepage-dashboard.enable = true;
+  # calibre-web.enable = true;
+  # jellyfin.enable = true;
+    prowlarr.enable = true;
+  # jackett.enable = true;
+    sonarr.enable = true;
+    radarr.enable = true;
+  # readarr.enable = true;
+  # bazarr.enable = true;
+    deluge.enable = true;
+    sabnzbd.enable = true;
+  # inadyn.enable = true;
+    gatus.enable = true;
+    # prometheus.enable = true;
+    # promtail.enable = true;
+    # loki.enable = true;
+    # grafana.enable = true;
+  # samba.enable = true;
+  # paperless.enable = true;
+    postgresql.enable = true;
+    postgresqlBackup.enable = true;
   };
 
+  systemd.tmpfiles.rules = lib.mkIf config.services.syncthing.enable [
+      "d ${config.custom.services.syncthing.syncDir} 0770 ${config.services.syncthing.user} ${config.customGlobal.dataGroup} -"
+  ];
 
   custom = {
     profiles.systemServices = {
       core.enable = true;
       server.enable = true;
     };
+
+    services = {
+
+      syncthing = {
+        syncDir = "${config.customHostSpecificGlobalOptions.pathToDataDirectory}/syncthing";
+        user = "syncthing";
+        simpleFileVersioningForBackUpMachinesOnly = {
+          type = "simple";
+          params = {
+            keep = "5";
+            cleanoutDays = "10";
+          };
+          cleanupIntervalS = 3600;
+        };
+
+        settings = {
+          devices = {
+            home-desktop.enable = true;
+            work-pc.enable = true;
+          };
+
+          folders = {
+            taskwarrior = {
+              enable = true;
+              devices = [
+                "${config.services.syncthing.settings.devices.home-desktop.name}"
+                "${config.services.syncthing.settings.devices.work-pc.name}"
+              ];
+            };
+
+            database = {
+              enable = true;
+              devices = [
+                "${config.services.syncthing.settings.devices.home-desktop.name}"
+              ];
+            };
+
+            documents = {
+              enable = true;
+              devices = [
+                "${config.services.syncthing.settings.devices.home-desktop.name}"
+              ];
+            };
+
+            study = {
+              enable = true;
+              devices = [
+                "${config.services.syncthing.settings.devices.home-desktop.name}"
+              ];
+            };
+
+            dev_resources = {
+              enable = true;
+              devices = [
+                "${config.services.syncthing.settings.devices.home-desktop.name}"
+                "${config.services.syncthing.settings.devices.work-pc.name}"
+              ];
+            };
+          };
+        };
+      };
+    };
+
+
   };
 }
