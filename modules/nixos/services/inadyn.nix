927712d08905e0f31f2defc307b50072e9073de4
Kobi Medrish <kobi.medrish@gmail.com>
2026-02-22 19:19:29 +0200

    Updated ddns

--- modules/nixos/services/inadyn.nix
+++ modules/nixos/services/inadyn.nix
@@ -1,7 +1,12 @@
 { config, lib, ... }:
 {
   config = lib.mkIf config.services.inadyn.enable {
-    sops.secrets."inadyn-dynu" = {
+    sops.secrets."inadyn/dynu" = {
+      owner = config.services.inadyn.user;
+      mode = "0600";
+    };
+
+    sops.secrets."inadyn/cloudflare" = {
       owner = config.services.inadyn.user;
       mode = "0600";
     };
@@ -9,10 +14,18 @@
     services.inadyn = {
       logLevel = "debug";
       settings = {
-         custom.dynu = {
-          include = config.sops.secrets.inadyn-dynu.path;
-           ddns-server = "api.dynu.com";
-           ddns-path = "/nic/update?username=%u&password=%p&myip=%i&hostname=%h"; # Correct path with variables
+        period = 300;
+
+        # Dynu stays custom (because it's NIC-style)
+        custom.dynu = {
+          include = config.sops.secrets."inadyn/dynu".path;
+          ddns-server = "api.dynu.com";
+          ddns-path = "/nic/update?username=%u&password=%p&myip=%i&hostname=%h";
+        };
+
+        # Cloudflare is a *provider*, not a custom section
+        provider."cloudflare.com" = {
+          include = config.sops.secrets."inadyn/cloudflare".path;
         };
       };
     };
