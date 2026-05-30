81e5039c381605d4d2fc55f7c2c4ce12d917d1e6
Kobi Medrish <kobi.medrish@gmail.com>
2026-05-30 18:39:18 +0300

    bumped home-manger state version

--- modules/home-manager/core.nix
+++ modules/home-manager/core.nix
@@ -18,6 +18,6 @@
     # You can update Home Manager without changing this value. See
     # the Home Manager release notes for a list of state version
     # changes in each release.
-    stateVersion = "25.05";
+    stateVersion = "26.05";
   };
 }
